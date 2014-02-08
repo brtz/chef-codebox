7a067df9e99889468da42e82b18ce49169158bc9
Nils Bartels <n.bartels@bigpoint.net>
2014-02-08 15:05:19 +0100

    [ADD] calling further components; [CHANGE] a lot of other stuff;

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 # Encoding: UTF-8
-default['codebox']['checkouts'] = []
+default['codebox']['data_bags']['secret_path'] = '/etc/chef/encrypted_data_bag_secret'
+default['codebox']['data_bags']['users_name'] = 'users'
+default['codebox']['data_bags']['projects_name'] = 'projects'
+default['codebox']['further_components'] = ['kitchen-env']
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,13 @@
 include_recipe 'codebox::_prerequisites'
 
 # install defined further components
+node['codebox']['further_components'].each do |component|
+  include_recipe 'codebox::_fc_' + component
+end unless node['codebox']['further_components'].empty?
+
+# lets get our data bags
+db_users = data_bag_item('codebox', node['codebox']['data_bags']['users_name'])
+db_projects = data_bag_item('codebox', node['codebox']['data_bags']['projects_name'])
 
 # we need to seperate users
 
